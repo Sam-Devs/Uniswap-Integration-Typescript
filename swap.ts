fa34bb528d2f33cf29b331f6f87e2fc905aed9a5
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-22 13:54:45 +0100

    swap.ts

--- swap.ts
+++ swap.ts
@@ -2,13 +2,26 @@ require("dotenv").config();
 import { ethers } from "ethers";
 import { Token } from "@uniswap/sdk-core";
 import { abi as ISwapRouter } from "@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json";
-import {  } from "@uniswap/v3-sdk";
+import { } from "@uniswap/v3-sdk";
 
 // Provider
 const mainnet = process.env.MAINNET;
 const provider = new ethers.providers.JsonRpcProvider(mainnet);
-const swapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
-const routerContract = new ethers.Contract(swapRouterAddress, ISwapRouter, provider);
+const routerAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+const routerContract = new ethers.Contract(routerAddress, ISwapRouter, provider);
 
 // Signer
-const 
+const signer = ethers.Wallet.createRandom();
+const account = signer.connect(provider);
+
+const swapRouter = routerContract.connect(account);
+
+// Token Address Object
+const tokenAddresses = {
+    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    token1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+};
+
+async function swap() {
+    
+}
