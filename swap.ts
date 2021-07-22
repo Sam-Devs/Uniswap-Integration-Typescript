6266433513d764cc1a37e1404784f677bc574a56
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-22 13:41:46 +0100

    swap.ts

--- swap.ts
+++ swap.ts
@@ -2,3 +2,13 @@ require("dotenv").config();
 import { ethers } from "ethers";
 import { Token } from "@uniswap/sdk-core";
 import { abi as ISwapRouter } from "@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json";
+import {  } from "@uniswap/v3-sdk";
+
+// Provider
+const mainnet = process.env.MAINNET;
+const provider = new ethers.providers.JsonRpcProvider(mainnet);
+const swapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+const routerContract = new ethers.Contract(swapRouterAddress, ISwapRouter, provider);
+
+// Signer
+const 
